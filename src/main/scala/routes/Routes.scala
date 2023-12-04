9578fcd977fd69c0ca5c83bdc430efe7a5904617
szczepania <szczepanikanja@gmail.com>
2023-12-04 11:56:14 +0100

    Error handling in route

--- src/main/scala/routes/Routes.scala
+++ src/main/scala/routes/Routes.scala
@@ -1,7 +1,8 @@
 package routes
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
 import services.{MoviesService, ScreeningRoomsService, ScreeningsService}
 
 import scala.concurrent.ExecutionContext
@@ -23,21 +24,35 @@ class Routes(
   private val seatsRouter = new SeatsRoute(seats)
   private val reservationsRouter = new ReservationsRoute(reservations)
 
-  val route: Route =
-    concat(
-      pathPrefix("healthcheck") {
-        get {
-          complete("200 -> health OK")
+  implicit def exceptionHandler: ExceptionHandler =
+    ExceptionHandler {
+      case e: NoSuchElementException =>
+        extractUri { uri =>
+          complete(StatusCodes.NotFound, s"Resource not found at $uri, error message: ${e.getMessage}")
         }
-      },
-      pathPrefix("api") {
-        concat(
-          moviesRouter.route,
-          screeningRoomsRouter.route,
-          screeningsRouter.route,
-          seatsRouter.route,
-          reservationsRouter.route
-        )
-      }
-    )
+      case e: Exception =>
+        extractUri { uri =>
+          complete(StatusCodes.InternalServerError, s"An error occurred at $uri, error message: ${e.getMessage}")
+        }
+    }
+
+  val route: Route =
+    handleExceptions(exceptionHandler) {
+      concat(
+        pathPrefix("healthcheck") {
+          get {
+            complete("200 -> health OK")
+          }
+        },
+        pathPrefix("api") {
+          concat(
+            moviesRouter.route,
+            screeningRoomsRouter.route,
+            screeningsRouter.route,
+            seatsRouter.route,
+            reservationsRouter.route
+          )
+        }
+      )
+    }
 }
